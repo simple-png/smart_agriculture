9c6dc870ff9a86df8efb39fee4cf6f3046ee3143
simple-png <61924870+simple-png@users.noreply.github.com>
2023-10-10 02:52:40 +0800

    删除冗余的log

--- src/main/java/com/agriculture/interceptor/JwtTokenUserInterceptor.java
+++ src/main/java/com/agriculture/interceptor/JwtTokenUserInterceptor.java
@@ -46,9 +46,7 @@ public class JwtTokenUserInterceptor implements HandlerInterceptor {
 
         //2、校验令牌
         try {
-            log.info("jwt校验:{}", token);
             Claims claims = JwtUtil.parseJWT(jwtProperties.getUserSecretKey(), token);
-            log.info("claims:{}",claims);
             Long userId = Long.valueOf(claims.get(JwtClaimsConstant.USER_ID).toString());
             log.info("当前用户id:{}", userId);
             //3、通过，放行
